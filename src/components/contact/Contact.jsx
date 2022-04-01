da290e7d254a3b6f95c11beedc982f950b843d61
SaidAoussar <aoussarsaid33@gmail.com>
2022-04-01 09:54:45 +0000

    create netlify form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,41 @@
-import { Col, Row } from "antd";
+import { Button, Col, Row, Typography } from "antd";
 import "./Contact.css";
+
+const { Title, Link } = Typography;
 
 function Contact() {
   return (
     <div>
       <Row justify="center">
-        <Col span={10}>Contact</Col>
+        <Col span={10}>
+          <Title level={5} style={{ fontWeight: "700" }}>
+            Feel free to email me via{" "}
+            <Link href="mailto:saidaoussar1998@gmail.com" target="_top">
+              saidaoussar1998@gmail.com
+            </Link>
+            <form name="contact" method="post" netlify="true" onSubmit="submit">
+              <input type="hidden" name="form-name" value="contact" />
+              <div>
+                <label>
+                  Full name:
+                  <input type="text" name="full-name" />
+                </label>
+              </div>
+              <div>
+                <label htmlFor="email">Email</label>
+                <input id="email" type="email" name="email" />
+              </div>
+              <div>
+                <label>
+                  Message:
+                  <textarea name="message"></textarea>
+                </label>
+              </div>
+              <button type="submit">Send</button>
+            </form>
+          </Title>
+          <Title></Title>
+        </Col>
       </Row>
     </div>
   );
