e4ace52f2b745554f6e60e2d272c112ef2ffa25a
SaidAoussar <aoussarsaid33@gmail.com>
2022-04-08 12:22:33 +0000

    add real projects informations

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,11 @@
-import { Row, Col, Card, Space } from "antd";
+import { Row, Col, Card, Space, Tag, Divider, Image } from "antd";
 import { GithubOutlined, EyeOutlined } from "@ant-design/icons";
+import projectsData from "../../data/projects.json";
 import "./Projects.css";
 
 const { Meta } = Card;
 function Projects() {
+  console.log(JSON.stringify(projectsData));
   return (
     <div className="projects" id="projects">
       <Space
@@ -16,87 +18,49 @@ function Projects() {
         gutter={[24, 16]}
         style={{ padding: "0px 100px", marginLeft: "0px", marginRight: "0px" }}
       >
-        <Col span={8}>
-          <Card
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
-            }
-            actions={[
-              <GithubOutlined key="github" />,
-              <EyeOutlined key="demo" />,
-            ]}
-          >
-            <Meta title="Card title" description="This is the description" />
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card
-            hoverable
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
-            }
-            actions={[
-              <GithubOutlined key="github" />,
-              <EyeOutlined key="demo" />,
-            ]}
-          >
-            <Meta title="Card title" description="This is the description" />
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
-            }
-            actions={[
-              <GithubOutlined key="github" />,
-              <EyeOutlined key="demo" />,
-            ]}
-          >
-            <Meta title="Card title" description="This is the description" />
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
-            }
-            actions={[
-              <GithubOutlined key="github" />,
-              <EyeOutlined key="demo" />,
-            ]}
-          >
-            <Meta title="Card title" description="This is the description" />
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card
-            cover={
-              <img
-                alt="example"
-                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-              />
-            }
-            actions={[
-              <GithubOutlined key="github" />,
-              <EyeOutlined key="demo" />,
-            ]}
-          >
-            <Meta title="Card title" description="This is the description" />
-          </Card>
-        </Col>
+        {projectsData.map((project) => {
+          return (
+            <Col lg={8} md={12} sm={24}>
+              <Card
+                cover={
+                  project.img ? (
+                    <Image
+                      height={300}
+                      src={require(`../../assets/img/projects/${project.img}`)}
+                    />
+                  ) : (
+                    <Image
+                      height={300}
+                      src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                    />
+                  )
+                }
+                // actions={[
+                //   <GithubOutlined key="github" />,
+                //   <EyeOutlined key="demo" />,
+                // ]}
+              >
+                <Meta title={project.title} description={project.description} />
+                {project.with && (
+                  <>
+                    <Divider orientation="left">With:</Divider>
+                    <Space wrap>
+                      {project.with.map((name) => (
+                        <Tag>{name}</Tag>
+                      ))}
+                    </Space>
+                  </>
+                )}
+                <Divider orientation="left">technologies</Divider>
+                <Space wrap>
+                  {project.technologies.map((tech) => (
+                    <Tag color="cyan">{tech}</Tag>
+                  ))}
+                </Space>
+              </Card>
+            </Col>
+          );
+        })}
       </Row>
     </div>
   );
