a58a479efc5a3b1c7e00855cd6e4c7d0c13f9546
SaidAoussar <aoussarsaid33@gmail.com>
2022-10-06 19:50:04 +0100

    fixing navbar responsive

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,14 +2,24 @@ import { useState } from "react";
 import { MenuOutlined, CloseOutlined } from "@ant-design/icons";
 import classnames from "classnames";
 import { Link } from "react-scroll";
+import useBodyScrollLock from "../../hooks/useBodyScrollLock";
 
 import "./Nav.css";
 
 function Nav() {
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [isLocked, toggle] = useBodyScrollLock();
 
-  const navLinksClassname = classnames("nav-links", {
+  const navLinksClassname = classnames("nav-links", "hide-for-mobile", {
     "nav-active": toggleMenu
+  });
+
+  const overlayClassname = classnames("overlay", {
+    "overlay--d-block": toggleMenu
+  });
+
+  const navMenuClassName = classnames("nav-menu", "has-fade", {
+    "fade-in": toggleMenu
   });
 
   const menuItems = [
@@ -32,6 +42,7 @@ function Nav() {
   ];
   return (
     <nav>
+      <div className={overlayClassname}></div>
       <div className="container">
         <div className="logo">
           <h4>{`<AOUSSAR/>`}</h4>
@@ -53,7 +64,34 @@ function Nav() {
             );
           })}
         </ul>
-        <div className="menu-icon" onClick={() => setToggleMenu(!toggleMenu)}>
+
+        <ul className={navMenuClassName}>
+          <li>
+            {menuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Link
+                    to={item.title.toLowerCase()}
+                    spy={true}
+                    offset={-50}
+                    smooth={true}
+                    duration={500}
+                    onClick={() => setToggleMenu(!toggleMenu)}
+                  >
+                    {item.title}
+                  </Link>
+                </li>
+              );
+            })}
+          </li>
+        </ul>
+        <div
+          className="menu-icon"
+          onClick={() => {
+            setToggleMenu(!toggleMenu);
+            toggle();
+          }}
+        >
           {toggleMenu ? (
             <CloseOutlined style={{ fontSize: "20px", color: "white" }} />
           ) : (
