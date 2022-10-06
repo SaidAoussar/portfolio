8f8b9a5a60c87e48d52cb3652ecd92581550d860
SaidAoussar <aoussarsaid33@gmail.com>
2022-10-06 11:32:40 +0100

    add container

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -8,47 +8,49 @@ function Nav() {
   const [toggleMenu, setToggleMenu] = useState(false);
 
   const navLinksClassname = classnames("nav-links", {
-    "nav-active": toggleMenu,
+    "nav-active": toggleMenu
   });
 
   const menuItems = [
     {
       title: "Home",
-      href: "#home",
+      href: "#home"
     },
     {
       title: "Skills",
-      href: "#skills",
+      href: "#skills"
     },
     {
       title: "Project",
-      href: "#projects",
+      href: "#projects"
     },
     {
       title: "Contact",
-      href: "#contact",
-    },
+      href: "#contact"
+    }
   ];
   return (
     <nav>
-      <div className="logo">
-        <h4>The Logo</h4>
-      </div>
-      <ul className={navLinksClassname}>
-        {menuItems.map((item, index) => {
-          return (
-            <li key={index}>
-              <a href={item.href}>{item.title}</a>
-            </li>
-          );
-        })}
-      </ul>
-      <div className="menu-icon" onClick={() => setToggleMenu(!toggleMenu)}>
-        {toggleMenu ? (
-          <CloseOutlined style={{ fontSize: "20px", color: "white" }} />
-        ) : (
-          <MenuOutlined style={{ fontSize: "20px", color: "white" }} />
-        )}
+      <div className="container">
+        <div className="logo">
+          <h4>{`<AOUSSAR/>`}</h4>
+        </div>
+        <ul className={navLinksClassname}>
+          {menuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a href={item.href}>{item.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="menu-icon" onClick={() => setToggleMenu(!toggleMenu)}>
+          {toggleMenu ? (
+            <CloseOutlined style={{ fontSize: "20px", color: "white" }} />
+          ) : (
+            <MenuOutlined style={{ fontSize: "20px", color: "white" }} />
+          )}
+        </div>
       </div>
     </nav>
   );
