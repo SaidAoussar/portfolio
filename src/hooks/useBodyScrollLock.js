e4e327101a6fcd582aff4063964f734560becfc3
SaidAoussar <aoussarsaid33@gmail.com>
2022-10-06 19:49:32 +0100

    create hook for noscroll

---
+++ src/hooks/useBodyScrollLock.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+const useBodyScrollLock = () => {
+  const bodyStyle = document.body.style;
+  const [isLocked, setIsLocked] = useState(bodyStyle.overflowY == "hidden");
+
+  useEffect(() => {
+    bodyStyle.overflowY = isLocked ? "hidden" : "auto";
+  }, [isLocked, bodyStyle]);
+
+  const toggle = () => setIsLocked(!isLocked);
+
+  return [isLocked, toggle];
+};
+
+export default useBodyScrollLock;
