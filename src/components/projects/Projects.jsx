c2e37284e825b04cf5303a8e5a6b74358e0cd0d0
SaidAoussar <aoussarsaid33@gmail.com>
2022-10-07 21:45:33 +0100

    fixing responsive card after adding container

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -13,50 +13,58 @@ function Projects() {
       >
         <h1>Projects</h1>
       </Space>
-      <Row
-        gutter={[24, 16]}
-        style={{ padding: "0px 100px", marginLeft: "0px", marginRight: "0px" }}
-      >
-        {projectsData.map((project) => {
-          return (
-            <Col lg={8} md={12} sm={24}>
-              <Card
-                cover={
-                  project.img ? (
-                    <Image
-                      height={300}
-                      src={require(`../../assets/img/projects/${project.img}`)}
-                    />
-                  ) : (
-                    <Image
-                      height={300}
-                      src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                    />
-                  )
-                }
-              >
-                <Meta title={project.title} description={project.description} />
-                {project.with && (
-                  <>
-                    <Divider orientation="left">With:</Divider>
-                    <Space wrap>
-                      {project.with.map((name) => (
-                        <Tag>{name}</Tag>
-                      ))}
-                    </Space>
-                  </>
-                )}
-                <Divider orientation="left">technologies</Divider>
-                <Space wrap>
-                  {project.technologies.map((tech) => (
-                    <Tag color="cyan">{tech}</Tag>
-                  ))}
-                </Space>
-              </Card>
-            </Col>
-          );
-        })}
-      </Row>
+      <div className="container">
+        <Row
+          gutter={[24, 16]}
+          style={{
+            marginLeft: "0px",
+            marginRight: "0px"
+          }}
+        >
+          {projectsData.map((project) => {
+            return (
+              <Col lg={8} md={12} sm={24}>
+                <Card
+                  cover={
+                    project.img ? (
+                      <Image
+                        height={300}
+                        src={require(`../../assets/img/projects/${project.img}`)}
+                      />
+                    ) : (
+                      <Image
+                        height={300}
+                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                      />
+                    )
+                  }
+                >
+                  <Meta
+                    title={project.title}
+                    description={project.description}
+                  />
+                  {project.with && (
+                    <>
+                      <Divider orientation="left">With:</Divider>
+                      <Space wrap>
+                        {project.with.map((name) => (
+                          <Tag>{name}</Tag>
+                        ))}
+                      </Space>
+                    </>
+                  )}
+                  <Divider orientation="left">technologies</Divider>
+                  <Space wrap>
+                    {project.technologies.map((tech) => (
+                      <Tag color="cyan">{tech}</Tag>
+                    ))}
+                  </Space>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </div>
     </div>
   );
 }
