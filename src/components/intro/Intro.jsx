8607ff8dec0e86caa0bb7991942adff820baa260
SaidAoussar <aoussarsaid33@gmail.com>
2022-10-12 12:56:42 +0100

    add link to resume in drive google

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-scroll";
 import { Row, Col, Space, Button } from "antd";
 import "./Intro.css";
 import introvector from "../../assets/img/introvector.png";
@@ -14,11 +15,18 @@ function Intro() {
           </h1>
           <h2>I am a Full-Stack Developer</h2>
           <Space size="middle">
-            <Button href="#contact" type="primary" size="large">
-              Get In Touch
-            </Button>
-            <Button type="default" size="large">
-              Download Resume
+            <Link to="contact" smooth={true}>
+              <Button type="primary" size="large">
+                Get In Touch
+              </Button>
+            </Link>
+            <Button
+              href="https://drive.google.com/file/d/1LNS1YF8UiAfn4rVwkVDp8EB6V0LDFrnM/view?usp=sharing"
+              target="_blank"
+              type="default"
+              size="large"
+            >
+              Resume
             </Button>
           </Space>
         </Col>
