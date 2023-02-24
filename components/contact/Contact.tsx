9db0a61faaf1c764666707dbff1038429d6403a7
SaidAoussar <aoussarsaid33@gmail.com>
2023-02-24 13:42:27 +0100

    fix: fix the code of form netlify

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -1,4 +1,14 @@
-import { Space, Typography, Row, Col, Form, Input, theme, Button } from "antd";
+import {
+  Space,
+  Typography,
+  Row,
+  Col,
+  Form,
+  Input,
+  theme,
+  Button,
+  message
+} from "antd";
 import Link from "next/link";
 import Container from "components/container/Container";
 import { Section } from "./styles";
@@ -7,10 +17,50 @@ const { Title, Text } = Typography;
 const { Item } = Form;
 const { TextArea } = Input;
 
+function encode(data: any) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + `=` + encodeURIComponent(data[key]))
+    .join(`&`);
+}
+
 const Contact = () => {
+  const formName = `contact`;
   const { token } = theme.useToken();
+  const [messageApi, contextHolder] = message.useMessage();
+  const [form] = Form.useForm();
+
+  const handleSubmit = (values: any) => {
+    if (values[`bot-field`] === undefined) {
+      delete values[`bot-field`];
+    }
+
+    fetch(`/`, {
+      method: `POST`,
+      headers: { "Content-Type": `application/x-www-form-urlencoded` },
+      body: encode({
+        "form-name": formName,
+        ...values
+      })
+    })
+      .then(() => showSuccess())
+      .catch((error: any) => showError(error));
+  };
+
+  const showSuccess = () => {
+    messageApi.open({
+      type: "success",
+      content: "form submitted successfully"
+    });
+  };
+  const showError = (error: any) => {
+    messageApi.open({
+      type: "error",
+      content: "there was an error submitting the form"
+    });
+  };
   return (
     <Section id="contact" $bgColor={token.colorBgContainer}>
+      {contextHolder}
       <Container>
         <Space
           direction="horizontal"
@@ -30,15 +80,40 @@ const Contact = () => {
                 saidaoussar1998@gmail.com
               </Link>
             </Title>
+
+            {/*
+              This defines how your form is setup for the Netlify bots.
+              Users will not see or interact with this form.
+            */}
+            <form
+              name={formName}
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              hidden
+            >
+              <input type="text" name="full-name" />
+              <input type="email" name="email" />
+              <textarea name="message"></textarea>
+            </form>
+
             <Form
-              name="contact"
-              action="/success"
+              name="cf"
+              form={form}
               method="POST"
               layout="vertical"
               autoComplete="off"
-              data-netlify="true"
+              onFinish={handleSubmit}
             >
-              <input type="hidden" name="form-name" value="contact" />
+              {/* This is the hidden field that the netlify-honeypot uses. */}
+              <Form.Item
+                label="Don't fill this out"
+                className={`hidden`}
+                style={{ display: `none` }}
+                name="bot-field"
+              >
+                <Input type={`hidden`} />
+              </Form.Item>
+
               <Item label={<Text strong>Full Name :</Text>}>
                 <Input
                   name="full-name"
